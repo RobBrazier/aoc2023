03cff261935023d181755ecea786640b7056c1e9
Rob Brazier <git+github@brzr.co>
2023-12-01 17:19:49 +0000

    fix some linting issues on day 1

--- src/bin/01.rs
+++ src/bin/01.rs
@@ -6,7 +6,7 @@ pub fn part_one(input: &str) -> Option<u32> {
     for item in &items {
         let digits = item
             .chars()
-            .filter(|c| c.is_digit(10))
+            .filter(|c| c.is_ascii_digit())
             .collect::<Vec<char>>();
         let len = digits.len();
         if len > 0 {
@@ -19,50 +19,49 @@ pub fn part_one(input: &str) -> Option<u32> {
     Some(total)
 }
 
-fn find_digits(input: &str) -> Option<String> {
+fn find_digits(input: &str, names: bool) -> String {
     let mut result = String::new();
     let mut pointer = String::new();
     for c in input.chars().collect::<Vec<char>>().iter() {
-        if c.is_digit(10) {
+        if c.is_ascii_digit() {
             result.push_str(c.to_string().as_str());
         } else {
             pointer.push_str(c.to_string().as_str());
         }
-        let named = match pointer.as_str() {
-            x if x.contains("one") => "1",
-            x if x.contains("two") => "2",
-            x if x.contains("three") => "3",
-            x if x.contains("four") => "4",
-            x if x.contains("five") => "5",
-            x if x.contains("six") => "6",
-            x if x.contains("seven") => "7",
-            x if x.contains("eight") => "8",
-            x if x.contains("nine") => "9",
-            _ => "",
-        };
-        if named != "" {
-            result.push_str(named);
-            pointer = pointer.chars().last().unwrap().to_string();
+        if names {
+            let named = match pointer.as_str() {
+                x if x.contains("one") => Some("1"),
+                x if x.contains("two") => Some("2"),
+                x if x.contains("three") => Some("3"),
+                x if x.contains("four") => Some("4"),
+                x if x.contains("five") => Some("5"),
+                x if x.contains("six") => Some("6"),
+                x if x.contains("seven") => Some("7"),
+                x if x.contains("eight") => Some("8"),
+                x if x.contains("nine") => Some("9"),
+                _ => None,
+            };
+            if let Some(val) = named {
+                result.push_str(val);
+                pointer = pointer.chars().last().unwrap().to_string();
+            }
         }
     }
     // println!("result: {result}");
-    Some(result)
+    result
 }
 
 pub fn part_two(input: &str) -> Option<u32> {
     let items = input.trim_end().split('\n').collect::<Vec<&str>>();
-    let mut total = 0;
-    for item in &items {
-        let digits = find_digits(item).unwrap().chars().collect::<Vec<char>>();
-        let len = digits.len();
-        if len > 0 {
-            let value = format!("{}{}", digits[0], digits[len - 1]);
-            let addition = value.parse::<u32>().unwrap();
-            // println!("addition: {addition}");
-            total += addition;
-        }
-    }
-    Some(total)
+    let result = items
+        .iter()
+        .map(|item| find_digits(item, true))
+        .filter(|item| !item.is_empty())
+        .map(|x| x.chars().collect::<Vec<char>>())
+        .map(|x| format!("{}{}", x[0], x[x.len() - 1]))
+        .map(|x| x.parse::<u32>().unwrap())
+        .sum::<u32>();
+    Some(result)
 }
 
 #[cfg(test)]
